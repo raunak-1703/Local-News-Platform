69fff42e4c4cc2e1044f98427d68e26edcdfc797
Raunak Kumar <nodepoint3@gmail.com>
2025-12-13 18:09:48 +0530

    Upvotes toggle and trending posts logic

--- backend/src/controllers/postController.js
+++ backend/src/controllers/postController.js
@@ -1,4 +1,5 @@
 import Post from '../models/postModel.js';
+import { calculatedTrendingScore } from '../utils/trendingScore.js';
 
 export const createPost = async (req,res)=>{
     const {title,content,category,location,media} = req.body;
@@ -25,5 +26,43 @@ export const getAllPosts = async (req,res)=>{
         res.json({posts});
     } catch (error) {
         res.status(500).json({message:'Error fetching posts'});
+    }
+}
+
+export const toggleUpvote = async (req,res)=>{
+    const post = await Post.findById(req.params.id);
+
+    if(!post){
+        return res.status(404).json({message:'Post not found'})
+    }
+
+    const userId = req.user._id.toString()
+    const index = post.upvotes.findIndex((id)=>id.toString()===userId);
+
+    if(index === -1 ){
+        post.upvotes.push(req.user._id);
+    }
+    else{
+        post.upvotes.splice(index,1)
+    }
+
+    post.upvoteCount = post.upvotes.length;
+    await post.save();
+
+    res.json({upvoteCount:post.upvoteCount})
+}
+
+export const getTrendingPosts = async (req,res)=>{
+    try {
+        const posts = await Post.find().populate('author','name');
+         const sorted = posts.map((post)=>({
+            post,
+            score:calculatedTrendingScore(post)      
+         }))
+         .sort((a,b)=>b.score - a.score)
+         .map((item)=>item.post)
+         res.json(sorted.slice(0,10))
+    } catch (error) {
+        res.status(500).json({message:'Error fetching trending posts'})
     }
 }
--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createPost,getAllPosts } from '../controllers/postController.js';
+import { createPost,getAllPosts, getTrendingPosts, toggleUpvote} from '../controllers/postController.js';
 import protect from '../middleware/authMiddleware.js';
 
 const postRouter = express.Router();
 
 postRouter.post('/',protect,createPost);
+postRouter.get('/trending', getTrendingPosts);
 postRouter.get('/',getAllPosts);
+postRouter.post('/:id/upvote',protect,toggleUpvote);
 
 export default postRouter;
